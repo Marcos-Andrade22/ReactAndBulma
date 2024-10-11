20d42223ec5b39568f5411d5b55347da841bf62c
Marcos Andrade <mvpdeandrade@gmail.com>
2024-10-10 21:21:49 -0300

    Finaliza consultar unico e atualizar

--- CrudReactEFrameworkCss/src/Components/Pages/UniqueQueryPage.jsx
+++ CrudReactEFrameworkCss/src/Components/Pages/UniqueQueryPage.jsx
@@ -1,27 +1,66 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Button from "../Button";
+import TextInput from "../TextInput";
+import React, { useContext } from "react";
+import { AlunosContext } from "../../AlunosContext";
 
 const UniqueQueryPage = () => {
+  const { alunos, setAlunos } = useContext(AlunosContext);
+  const [id, setId] = React.useState("");
+  const [alunoUnico, setAlunoUnico] = React.useState(null)
+
   const navigate = useNavigate();
 
   const goToHomePage = () => {
     navigate("/");
   };
 
+  const limparAlunos = () =>{
+    setAlunos([]);
+  }
+
   const consultarUnico = (id) => {
     axios.get(`http://localhost:3005/alunos/${id}`).then((resultado) => {
-      <h1>resultado.data</h1>;
+      setAlunoUnico(resultado.data)
+    });
+  };
+
+  const listar = () => {
+    axios.get("http://localhost:3005/alunos").then((resultado) => {
+      setAlunos(resultado.data);
     });
   };
 
   return (
     <>
-      <h1>Listar um usuário:</h1>
-      <Button onClick={consultarUnico} label={"Consultar Único"}/>
+      <TextInput
+        onChange={(e) => setId(e.target.value)}
+        type="text"
+        value={id}
+        placeholder="Digite o id do usuário a ser consultado"
+      />
+      <Button onClick={() => consultarUnico(id)} label="Consultar Único" />
 
-      <Button onClick={goToHomePage} label={"Voltar para a página principal"} />
+      {alunoUnico && (
+        <div>
+          <h2>Informações do Aluno</h2>
+          <p>ID: {alunoUnico.id} </p>
+          <p>Nome: {alunoUnico.nome}</p>
+        </div>
+      )}
+      <Button onClick={goToHomePage} label="Voltar para a página principal" />
+      <Button onClick={listar} label="Listar" />
 
+      <ul>
+        {alunos.map((aluno) => (
+          <li key={aluno.id}>
+            ID: {aluno.id}
+          </li>
+        ))}
+      </ul>
+
+      <Button onClick={limparAlunos} label={"Limpar Alunos"}/>
     </>
   );
 };
--- CrudReactEFrameworkCss/src/Components/Pages/UpdatePage.jsx
+++ CrudReactEFrameworkCss/src/Components/Pages/UpdatePage.jsx
@@ -1,32 +1,67 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Button from "../Button";
+import React , { useContext, useState } from "react";
+import TextInput from "../TextInput";
+import { AlunosContext } from "../../AlunosContext";
 
 const UpdatePage = () => {
+  const { alunos, setAlunos } = useContext(AlunosContext);
   const navigate = useNavigate();
+  const [statusMessage, setStatusMessage] = React.useState("")
+  const [id, setId] = useState("")
 
   const goToHomePage = () => {
     navigate("/");
   };
 
-  const alterar = (id) => {
-    axios.get(`http://localhost:3005/alunos/${id}`).then((resultado) => {
-      let aluno = resultado.data;
-      aluno.nome += " (alterado)"
-        .put(`http://localhost:3005/alunos/${id}`, aluno)
-        .then((resultado) => {
-          <h1>resultado.status</h1>;
-        });
+  const listar = () => {
+    axios.get("http://localhost:3005/alunos").then((resultado) => {
+      setAlunos(resultado.data);
     });
+  };
+
+  const alterar = (id) => {
+    axios.get(`http://localhost:3005/alunos/${id}`)
+      .then((resultado) => {
+        let aluno = resultado.data;
+        aluno.nome += " (alterado)"
+        return axios.put(`http://localhost:3005/alunos/${id}`, aluno)       
+      })
+      .then((putResultado) =>{
+        setStatusMessage(`Status da atualização: ${putResultado.status}`)
+      })
+      .catch((error) =>{
+        setStatusMessage(`Erro ao atualizar o aluno: ${error.message}`);
+      })
   };
 
   return (
     <>
       <h1>Update Page</h1>
-      <Button onClick={alterar} label={"Atualizar usuário"}/>
+
+      <TextInput
+        type="text"
+        placeholder="Digite o ID do aluno"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <Button onClick={() => alterar(id)} label={"Atualizar usuário"} />
+
+      <Button onClick={listar} label={"Listar"} />
+
+      <ul>
+        {alunos.map((aluno) => (
+          <li key={aluno.id}>
+            {" "}
+            ID: {aluno.id} - Nome: {aluno.nome} - Matrícula: {aluno.matricula}{" "}
+          </li>
+        ))}
+      </ul>
 
       <Button onClick={goToHomePage} label={"Voltar para a página principal"} />
 
+      {statusMessage && <p>{statusMessage}</p>}
     </>
   );
 };
